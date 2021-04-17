eec3f196663afa9bc49a6b4ee4d8a9a51629b7f6
coresic <2869789245@qq.com>
2021-04-17 22:35:48 +0800

    增加按键控制不同功能

--- main.c
+++ main.c
@@ -18,6 +18,9 @@ float wendu;//温度数据
 uint count;//定时器计数值
 uchar hour, minute, second;//时分秒
 uchar ADC_data;//ADC数据
+//uchar s4, s5, s6, s7;//四个独立按键
+uchar key_data;//按键扫描值
+sbit key=P4^4;
 
 void Enable38(uchar num)//38译码器
 {
@@ -53,6 +56,11 @@ void Display_wendu()//温度显示
 	WEI[0]=DUAN[wendu_num/100];
 	WEI[1]=DUAN[wendu_num/10%10]&0x7f;
 	WEI[2]=DUAN[wendu_num%10];
+	WEI[3]=0xff;
+	WEI[4]=0xff;
+	WEI[5]=0xff;
+	WEI[6]=0xff;
+	WEI[7]=0xff;
 }
 
 void Display_date()//时间显示
@@ -75,7 +83,7 @@ void Display_date()//时间显示
 	WEI[7]=DUAN[second%10];
 }
 
-void Display_ADC(unsigned char address_ADC)
+void Display_ADC(unsigned char address_ADC)//ADC读取数据显示
 {
 	ADC_data=PCF8591_Get_ADC(address_ADC);
 	WEI[0]=DUAN[ADC_data/100];
@@ -88,7 +96,7 @@ void Display_ADC(unsigned char address_ADC)
 	WEI[7]=0xff;
 }
 
-void Display_AT24C02(unsigned char num)
+void Display_AT24C02(unsigned char num)//AT24C02读取数据显示
 {
 	WEI[0]=DUAN[num/100];
 	WEI[1]=DUAN[num/10%10];
@@ -128,35 +136,60 @@ void LED8_OFF(uchar num)//流水灯关
 	P0=0Xff;
 }
 
+void Key_scan()//按键扫描
+{
+	key=0;
+	P3|=0x0f;
+	switch(P3&0x0f)
+	{
+		case 0x0e:key_data=7;break;
+		case 0x0d:key_data=6;break;
+		case 0x0b:key_data=5;break;
+		case 0x07:key_data=4;break;
+	}
+	while(P3&0x0f!=0x0f);
+	
+}
+
 void main()//主函数
 {
 	float temp_num;
+	bit flag_led;
 	Display_init();//数码管显示初始化
 	Timer0_Init();//定时器0初始化
 	Init_DS1302();//DS1302初始化
 	LED8_OFF(8);//关闭8个led
-	AT24C02_write(0x03,60);
-//	adc_set(0);
-	Display_AT24C02(AT24C02_read(0x03));
+	AT24C02_write(0x03,60);//AT24C02写入数据
+//	adc_set(0);//DA输出
+	Display_AT24C02(AT24C02_read(0x03));//AT24C02读取数据显示
 	Display_AT24C02(AT24C02_read(0x03));//重复一次，保证读取成功率
 	while(1)
 	{
 		
-		if(count>=100)
+		if(count>=20)
 		{
 			count=0;
-			
-//			Display_ADC(0x01);//ADC数据显示
-			
-//			Display_date();//时间显示
-			
-//			temp_num=Ds18b20_Get_data();//获取温度值
-//			if(temp_num<30)
-//			wendu=temp_num;//更新温度值
-//			Display_wendu();//更新温度显示
+			if(key_data!=0)
+			{
+				switch(key_data)
+				{
+					case 4:	Display_ADC(0x01);//ADC数据显示
+							break;
+					case 5:	Display_date();//时间显示
+							break;
+					case 6:	temp_num=Ds18b20_Get_data();//获取温度值
+							if(temp_num<30)
+							wendu=temp_num;//更新温度值
+							Display_wendu();//更新温度显示
+							break;
+					case 7:	flag_led=!flag_led;
+							if(flag_led)LED8_ON(1);else LED8_OFF(1);//按键控制led
+							break;
+				}
+				
+//				key_data=0;
+			}
 		}
-		
-		
 	}
 }
 
@@ -165,5 +198,6 @@ void Timer0() interrupt 1//定时器0中断
 	TH0=-9;
 	count++;
 	Display();//数码管显示函数
+	Key_scan();//按键扫描
 }
 
